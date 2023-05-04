e49214f98f7b2c2863203e95638e8f8308ae12fe
Tony Pai <tony77794@gmail.com>
2023-05-04 23:27:11 +0800

    Update main.py with improved retrieval chain and UI messaging

--- main.py
+++ main.py
@@ -10,7 +10,7 @@ embeddings = OpenAIEmbeddings()
 db = FAISS.load_local("faiss_index", embeddings)
 
 # Create a retrieval chain with the ChatOpenAI model
-chain = RetrievalQAWithSourcesChain.from_chain_type(llm=ChatOpenAI(temperature=0), chain_type="stuff", retriever=db.as_retriever(search_kwargs={"k": 2}))
+chain = RetrievalQAWithSourcesChain.from_chain_type(llm=ChatOpenAI(model_name="gpt-4", temperature=0), chain_type="stuff", retriever=db.as_retriever(), max_tokens_limit=2500, reduce_k_below_max_tokens=True)
 
 # Streamlit input field and text area
 st.title("台灣法規問答 AI")
@@ -19,10 +19,10 @@ st.markdown("""
 """)
 # Description block
 st.markdown("""
-本工具引用自全國法規資料庫之民法、中華民國刑法、刑事訴訟法、勞動基準法、勞工退休金條例以及職業安全衛生設施條例之 PDF 檔案，本工具僅供研究和學習使用，如有法律需求請諮詢專業律師。
+本工具引用自全國法規資料庫之[民法](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=B0000001)、[中華民國刑法](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=C0000001)、[刑事訴訟法](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=C0010001)、[勞動基準法](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=N0030001)、[勞工退休金條例](https://law.moj.gov.tw/LawClass/LawAll.aspx?pcode=N0030020)以及[職業安全衛生設施條例](https://law.moj.gov.tw/Hot/AddHotLaw.ashx?pcode=N0060009)之 PDF 檔案，本工具僅供研究和學習使用，如有法律需求請諮詢專業律師。
 """)
 question = st.text_input("請輸入您的問題：")
 if question:
     with st.spinner("🤖 思考中，請稍候..."):
-        output = chain({"question": f"請用台灣繁體中文回答問題：{question}"}, return_only_outputs=True)
+        output = chain({"question": f"{question} 請用台灣繁體中文回答"}, return_only_outputs=True)
     st.text_area("🤖：", value=output["answer"], height=200)
